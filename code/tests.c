f7f67750364e2a2111d4c6dd606b27b316f917a0
ewalks2332 <emilywalker2332@gmail.com>
2018-03-06 18:31:24 -0500

    Added barebones test class

---
+++ code/tests.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "CUnit.h"
+#include "Basic.h"
+#include "code.h"
+
+void test00(void) {}
+
+/* The main() function for setting up and running the tests.
+ * Returns a CUE_SUCCESS on successful running, another
+ * CUnit error code on failure.
+ */
+int main()
+{
+   CU_pSuite pSuite = NULL;
+
+   /* initialize the CUnit test registry */
+   if (CUE_SUCCESS != CU_initialize_registry()) { return CU_get_error(); }
+
+   /* add a suite to the registry */
+   pSuite = CU_add_suite("Suite_1", NULL, NULL);
+   if (NULL == pSuite) {
+      CU_cleanup_registry();
+      return CU_get_error();
+   }
+
+   /* add the tests to the suite */
+   if (
+          (NULL == CU_add_test(pSuite, "test00", test00))
+      )
+   {
+      CU_cleanup_registry();
+      return CU_get_error();
+   }
+
+   /* Run all tests using the CUnit Basic interface */
+   CU_basic_set_mode(CU_BRM_VERBOSE);
+   CU_basic_run_tests();
+   //   CU_basic_show_failures(CU_get_failure_list());
+   CU_cleanup_registry();
+   return CU_get_error();
+}
