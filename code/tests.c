f97c92b74841de1e181154de329651f4d4c74bbd
Kenny <kzhao7301@gmail.com>
2018-03-18 16:10:08 -0400

    working on reproducing subtract bug in testcases

--- code/tests.c
+++ code/tests.c
@@ -2,6 +2,7 @@
 #include "CUnit.h"
 #include "Basic.h"
 #include "defs.h"
+#include "REPL.h"
 
 void check_createStack();
 void check_createEnv();
@@ -10,6 +11,24 @@ void check_createEnvList();
 void test00(void) { check_createStack(); }
 void test01(void) { check_createEnv(); }
 void test02(void) { check_createEnvList(); }
+
+void subtractTest(void){
+   struct ExprList *stack = exprList_Empty();
+   struct BindList *env = bindList_Empty();
+   struct BindListList *envList = bindListList_Empty();
+   bindListList_push(envList,env);
+
+   Struct Expr * a = parse("5", envList);
+   eval(a, stack, envList); 
+   Struct Expr * b = parse("7", envList); 
+   eval(b, stack, envList); 
+   Struct Expr * sub = parse("sub", envList); 
+   eval(sub, stack, envList); 
+   
+   printf("Subtract test: Stack is [5 7 sub] with actual result of %d and expected is %d\n", exprList_top(stack).subtype.number.value, 2);
+   CU_ASSERT(exprList_top(stack).type == NUMBER && exprList_top(stack).subtype.number.value == 2);
+
+}
 
 void check_createStack() {
 	struct ExprList *stack = exprList_Empty();
@@ -31,6 +50,8 @@ void check_createEnvList() {
         CU_ASSERT_PTR_NULL(bindListList_pop(envList));
 }
 
+
+
 /* The main() function for setting up and running the tests.
  * Returns a CUE_SUCCESS on successful running, another
  * CUnit error code on failure.
@@ -51,9 +72,10 @@ int main()
 
    /* add the tests to the suite */
    if (
-        	(NULL == CU_add_test(pSuite, "createStackTest", test00))
-	||	(NULL == CU_add_test(pSuite, "createEnvTest", test01))
-	||	(NULL == CU_add_test(pSuite, "createEnvListTest", test02))
+         (NULL == CU_add_test(pSuite, "createStackTest", test00))
+	  || (NULL == CU_add_test(pSuite, "createEnvTest", test01))
+	  || (NULL == CU_add_test(pSuite, "createEnvListTest", test02))
+     || (NULL == CU_add_test(pSuite, "subtractTest", subtractTest))
       )
    {
       CU_cleanup_registry();
